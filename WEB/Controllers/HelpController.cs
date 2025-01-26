9ee938b21e8152cc73d8e647605ab7b755847f17
Литвиненко Влад <vladlit.vinenko2909@gmail.com>
2025-01-26 18:46:35 +0200

    Added Request confirmation page

---
+++ WEB/Controllers/HelpController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB.Models; // Импорт модели, если потребуется
+
+public class HelpController : Controller
+{
+    [HttpPost]
+    public IActionResult SubmitHelpForm(HelpFormModel model)
+    {
+        // Передаем данные в представление через ViewBag
+        ViewBag.FirstName = model.FirstName;
+        ViewBag.LastName = model.LastName;
+
+        // Отображаем страницу HelpConfirmation
+        return View("HelpConfirmation");
+    }
+}
---
+++ WEB/Models/HelpFormModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WEB.Models
+{
+    public class HelpFormModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
