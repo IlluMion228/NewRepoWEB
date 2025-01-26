6f2cdac546d0da86e5a5b8ffe522a980e700afbd
Литвиненко Влад <vladlit.vinenko2909@gmail.com>
2025-01-26 22:56:09 +0200

    Connected db

---
+++ WEB/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WEB.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Здесь добавляются DbSet для ваших моделей данных
+        public DbSet<Booking> Booking { get; set; }
+    }
+}
